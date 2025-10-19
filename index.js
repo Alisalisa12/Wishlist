b7abf723ad3e7ff84ce2eef4d0d27939caf1ba7d
A <alisazz805@gmail.com>
2025-10-19 17:04:01 +0300

    Добавлена регистрация

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import {validationResult} from 'express-validator';
+import { registerValidation } from './validation/auth.js';
+import UserModel from './models/User.js';
+
 mongoose.connect('mongodb+srv://admin:Z0rla03kFeEeVRtR@cluster0.fqgrana.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 .then (() => console.log('DB ok'))
 .catch(() => console.log('DB error', err));
@@ -10,23 +14,23 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-});
 
-app.post('/login', (req, res) => {
-    console.log(req.body);
-    const token = jwt.sign(
-        {
-            email: req.body.email,
-            fullName: 'Иван Иванов'
-        },
-        'key123',
-    );
+app.post('/auth/register', registerValidation, (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).json(errors.array());
+    }
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        passwordHash: req.body.passwordHash,
+        avatarUrl: req.body.avatarUrl,
+
+    });
 
     res.json({
         success: true,
-
     });
 });
 
